b182912210c1a5e71aedcc73d15ab4f24d74b5ac
AlgoQ <kobejanssens@outlook.com>
2023-11-28 14:16:26 +0100

    wildcard ALLOW_HOSTS

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -48,12 +48,10 @@ if not IS_HEROKU_APP:
 # validation of the Host header in the incoming HTTP request. On other platforms you may need
 # to list the expected hostnames explicitly to prevent HTTP Host header attacks. See:
 # https://docs.djangoproject.com/en/4.2/ref/settings/#std-setting-ALLOWED_HOSTS
-# if IS_HEROKU_APP:
-#     ALLOWED_HOSTS = ["*"]
-# else:
-#     ALLOWED_HOSTS = []
-
-ALLOWED_HOSTS = ['kobejanssens-9e7323cccf77.herokuapp.com', '127.0.0.1']
+if IS_HEROKU_APP:
+    ALLOWED_HOSTS = ["*"]
+else:
+    ALLOWED_HOSTS = []
 
 # Application definition
 
