2ee3afb6aca92d087abc89dc26272f62021001f6
AlgoQ <kobejanssens@outlook.com>
2023-11-28 12:15:23 +0100

    Update static settings section

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -150,7 +150,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'core', 'static'),
+]
 
 STORAGES = {
     # Enable WhiteNoise's GZip and Brotli compression of static assets:
